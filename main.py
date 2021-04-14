ea5e9d736bff3068f6ecd33bf633fbc2b5c254ee
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2021-04-14 11:47:49 +0800

    Removed TOKEN from public repo

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import os
 from pugbot.bot import bot
 
 # Default token retrieved from: https://discord.com/developers/applications/831476145063985162/bot
-bot.run(os.getenv('TOKEN', "ODMxNDc2MTQ1MDYzOTg1MTYy.YHVygQ.7u6LXRXrFZMQVehnYo3PzCvIAYg"))
+bot.run(os.getenv('TOKEN'))
