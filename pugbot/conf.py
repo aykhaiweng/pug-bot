135511d72fd29b3928d8e835f07580ddaf0b0743
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2021-04-20 15:54:16 +0800

    Cleaned up files

--- pugbot/conf.py
+++ pugbot/conf.py
@@ -14,14 +14,6 @@ DEFAULT_LOG_FORMATTER = os.getenv(
 )
 
 
-# PostgreSQL connection
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT")
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-
-
 # Discord Bot
 TOKEN = os.getenv("TOKEN")
 PREFIX = os.getenv("PREFIX", ".")  # Prefix for the commands
